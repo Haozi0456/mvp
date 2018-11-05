117e90766a3c530b08a0ec5ef8ed1122d0c3b9bd
zhaohao <hao.zhao@zhaogang.com>
2018-11-05 10:49:50 +0800

    1.添加请求取消方法
    1.添加请求取消方法

--- mvplibrary/src/main/java/com/zwh/mvp/library/base/request/OkHelper.java
+++ mvplibrary/src/main/java/com/zwh/mvp/library/base/request/OkHelper.java
@@ -26,13 +26,15 @@ public class OkHelper {
 
     /**
      * get 请求
+     * @param tag   请求的tag，用于标识当前的请求，方便后续取消对应的请求，如果你不需要取消请求，也可以不用设置
      * @param url 请求地址
      * @param params 参数键值对
      * @param callback 返回
      * @param <T>
      */
-    public static <T> void getRequest(String url, Map<String,String> params, JsonCallback<T> callback){
+    public static <T> void getRequest(Object tag,String url, Map<String,String> params, JsonCallback<T> callback){
         OkGo.<T>get(url)
+                .tag(tag)
                 .cacheKey(url)
                 .params(params)
                 .cacheMode(CacheMode.FIRST_CACHE_THEN_REQUEST)
@@ -42,39 +44,44 @@ public class OkHelper {
 
     /**
      * post 请求
+     * @param tag   请求的tag，用于标识当前的请求，方便后续取消对应的请求，如果你不需要取消请求，也可以不用设置
      * @param url 请求地址
      * @param params 参数键值对
      * @param callback 返回
      * @param <T>
      */
-    public static <T> void postRequest(String url, Map<String, String> params, JsonCallback<T> callback) {
+    public static <T> void postRequest(Object tag,String url, Map<String, String> params, JsonCallback<T> callback) {
         OkGo.<T>post(url)
+                .tag(tag)
                 .params(params)
                 .execute(callback);
     }
 
     /** json 请求
+     * @param tag 请求的tag，用于标识当前的请求，方便后续取消对应的请求，如果你不需要取消请求，也可以不用设置
      * @param url 请求地址
      * @param jsonStr json字符串
      * @param callback 返回
      * @param <T>
      */
-    public static <T> void jsonRequest(String url, String jsonStr, JsonCallback<T> callback) {
+    public static <T> void jsonRequest(Object tag,String url, String jsonStr, JsonCallback<T> callback) {
         OkGo.<T>post(url)
-//                .tag(context) // 请求的tag，用于标识当前的请求，方便后续取消对应的请求，如果你不需要取消请求，也可以不用设置
+                .tag(tag) // 请求的tag，用于标识当前的请求，方便后续取消对应的请求，如果你不需要取消请求，也可以不用设置
                 .upJson(jsonStr)
                 .execute(callback);
     }
 
     /**
+     * @param tag 请求的tag，用于标识当前的请求，方便后续取消对应的请求，如果你不需要取消请求，也可以不用设置
      * @param url 请求地址
      * @param params 参数键值对
      * @param filesParamsKey 文件上传参数名称
      * @param files 文件列表
      * @param callback 返回
      */
-    public static void upFilesRequest(String url, Map<String, String> params,String filesParamsKey, List<File> files, StringCallback callback){
+    public static void upFilesRequest(Object tag,String url, Map<String, String> params,String filesParamsKey, List<File> files, StringCallback callback){
         PostRequest<String> request = OkGo.<String>post(url)
+                .tag(tag)
                 .cacheMode(CacheMode.NO_CACHE)
                 .isMultipart(true)
                 .params(params);
@@ -85,14 +92,16 @@ public class OkHelper {
     }
 
     /**
+     * @param tag 请求的tag，用于标识当前的请求，方便后续取消对应的请求，如果你不需要取消请求，也可以不用设置
      * @param url 请求地址
      * @param params 参数键值对
      * @param filesParamsKey 文件上传参数名称
      * @param file 文件
      * @param callback 返回
      */
-    public static void upFileRequest(String url, Map<String, String> params,String filesParamsKey, File file, StringCallback callback){
+    public static void upFileRequest(Object tag,String url, Map<String, String> params,String filesParamsKey, File file, StringCallback callback){
          OkGo.<String>post(url)
+                 .tag(tag)
                 .cacheMode(CacheMode.NO_CACHE)
                 .isMultipart(true)
                 .params(params)
@@ -100,10 +109,30 @@ public class OkHelper {
                 .execute(callback);
     }
 
-    public static void downLoadFileRequest(String url, FileCallback callback){
+    /**
+     * @param tag 请求的tag，用于标识当前的请求，方便后续取消对应的请求，如果你不需要取消请求，也可以不用设置
+     * @param url
+     * @param callback
+     */
+    public static void downLoadFileRequest(Object tag,String url, FileCallback callback){
         OkGo.<File>get(url)
+                .tag(tag)
                 .execute(callback);
     }
 
+    /**
+     * 取消Tag请求
+     * @param tag
+     */
+    public static void cancelRequest(Object tag){
+        OkGo.getInstance().cancelTag(tag);
+    }
 
+
+    /**
+     * 取消全部请求
+     */
+    public static void cancelAllRequest(){
+        OkGo.getInstance().cancelAll();
+    }
 }
